a719278a8718664732c7deb7961c96d6fe18a8cc
hyoju0104 <hyoju8618@naver.com>
2025-08-04 15:38:43 +0900

    로그인,회원가입 html작성,urls.py,views.py수정

--- dreamlens_core/urls.py
+++ dreamlens_core/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
 
     # 꿈 조합기 페이지
     path('combine/', views.dream_combiner, name='dream_combiner'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
 ]
--- dreamlens_core/views.py
+++ dreamlens_core/views.py
@@ -1,6 +1,10 @@
 import os
 import openai
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -100,3 +104,46 @@ def dream_combiner(request):
             })
 
     return render(request, "combine.html", context)
+
+# 로그인, 회원가입 - 안주경
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        nickname = request.POST.get('nickname')
+
+        if password != password2:
+            messages.error(request, '비밀번호가 일치하지 않습니다.')
+            return redirect('register')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, '이미 존재하는 아이디입니다.')
+            return redirect('register')
+
+        user = User.objects.create_user(username=username, password=password)
+        user.first_name = nickname  # 닉네임을 first_name에 임시 저장
+        user.save()
+        return redirect('login')
+
+    return render(request, 'register-user.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')  # 메인 페이지로
+        else:
+            messages.error(request, '아이디 또는 비밀번호가 틀렸습니다.')
+
+    return render(request, 'login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
