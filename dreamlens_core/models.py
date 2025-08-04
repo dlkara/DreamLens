67dbf446d10d0125f7591a13039e99c2078b6aa6
dlkara <lhj020731@gmail.com>
2025-08-04 15:23:01 +0900

    꿈 사전 페이지 제작

--- dreamlens_core/models.py
+++ dreamlens_core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class DreamDict(models.Model):
+    category = models.CharField(max_length=50)   # 대분류
+    keyword = models.CharField(max_length=50)    # 소분류 키워드
+    meaning = models.TextField()                 # 상징적 의미
+
+    def __str__(self):
+        return f"[{self.category}] {self.keyword}"
--- dreamlens_core/urls.py
+++ dreamlens_core/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
 
-    # 꿈 조합기 페이지
+    # 꿈 사전
+    path('dict/', views.dream_dict_view, name='dream_dict_view'),
+
+    # 꿈 조합기
     path('combine/', views.dream_combiner, name='dream_combiner'),
 ]
